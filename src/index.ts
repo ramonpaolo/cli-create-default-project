640432f147203a0ad393540b4f51ccd606850624
Ramon Paolo Maram <ramonpaolomaran12@gmail.com>
2022-12-19 23:32:08 -0300

    feat: add examples

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import yargs from 'yargs'
 import { exec } from 'child_process'
 
 const argv = yargs
+    .example('create-project', 'create project with express')
+    .example('create-project --docker --http2 --cloud aws --database redis', 'create a project with docker, http2, nginx and install redis and aws-sdk')
     .option('http2', {
         type: 'boolean',
         description: 'Configure the project to use http 2.0(sdpy)'
