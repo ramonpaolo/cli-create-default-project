f24d1517e5c7018b8320c86683ec60cf57ded27f
Ramon Paolo Maram <ramonpaolomaran12@gmail.com>
2023-03-02 07:55:29 -0300

    feat: add default scripts and file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import { exec } from 'child_process'
+import { readFileSync, writeFileSync } from 'fs'
 
 // Utils
 import log from './utils/logger.utils'
@@ -19,7 +20,30 @@ const keysAvailables = Object.keys(argv) as KeysAvailables[]
 
 const baseUrl = 'wget https://raw.githubusercontent.com/ramonpaolo/default-files-script-automation/master/'
 
+const packageJsonTemplate = {
+    "version": "0.0.1",
+    "main": "dist/index.js",
+    "scripts": {
+      "dev": "nodemon -r dotenv/config src/index.ts",
+      "test": "NODE_ENV=test jest --verbose --coverage --forceExit",
+      "start": "node dist/index.js",
+      "build": "tsc"
+    },
+}
+
 exec('yarn init -y', () => {
+    const packageBuffer = readFileSync('./package.json', {
+        encoding: 'utf-8',
+    })
+
+    const packageJson = JSON.parse(packageBuffer)
+
+    packageJson['main'] = packageJsonTemplate.main
+    packageJson['scripts'] = packageJsonTemplate.scripts
+    packageJson['version'] = packageJsonTemplate.version
+
+    writeFileSync('./package.json', Buffer.from(JSON.stringify(packageJson)))
+
     createFolders(keysAvailables)
     downloadBasicFiles(baseUrl, argv, keysAvailables)
 })
